423793827fe78d22acad2ec6ea0d090cb3cca860
valkavernavmbm <valkaverna.vmbm@gmail.com>
2026-04-22 15:00:15 -0300

    Sync local code to GitHub

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Clientes from './pages/Clientes';
